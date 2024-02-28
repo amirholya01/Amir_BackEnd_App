2509d051be87ce05392abb8f749fe0436add59a0
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-28 19:43:23 +0100

    first commit

---
+++ src/main/java/com/amir/AmirBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.amir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmirBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmirBackEndApplication.class, args);
+	}
+
+}
