05d0890236a319817ddfafba48d0f0b97b1ef6f4
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-28 20:06:25 +0100

    enum-refactor

--- src/main/java/com/amir/enums/ERole.java
+++ src/main/java/com/amir/enums/ERole.java
@@ -1,7 +1,19 @@
 package com.amir.enums;
 
+/**
+ * Enum representing roles in the application.
+ */
 public enum ERole {
+    /**
+     * Represents a regular user role.
+     */
     ROLE_USER,
+    /**
+     * Represents an administrator role.
+     */
     ROLE_ADMIN,
+    /**
+     * Represents a moderator role.
+     */
     ROLE_MODERATOR
 }
