2d7c9c13ddbf210aef6d407a79d72275e1d22bc0
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-02-29 13:30:23 +0100

    commenting for security services

--- src/main/java/com/amir/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/amir/security/service/UserDetailsServiceImpl.java
@@ -8,6 +8,11 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+
+/**
+ * Implementation of the Spring Security UserDetailsService interface.
+ * This class is responsible for loading user-specific data.
+ */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
@@ -15,11 +20,21 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     UserRepository userRepository;
 
 
+
+    /**
+     * Load user by username.
+     * @param username The username of the user to be loaded.
+     * @return UserDetails object representing the loaded user.
+     * @throws UsernameNotFoundException If the user is not found.
+     */
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // Find user by username in the repository
         User user = userRepository.findByUsername(username).orElseThrow(
+                // Throw UsernameNotFoundException if user is not found
                 () -> new UsernameNotFoundException("User was not found " + username)
         );
+        // Build and return UserDetailsImpl object from the found user
         return UserDetailsImpl.build(user);
     }
 }
