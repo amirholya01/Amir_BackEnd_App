5e036d231a1bbe73198e142146338d9ed358e174
amirholya01 <120662275+amirhosseinolya@users.noreply.github.com>
2024-03-01 06:32:04 +0100

    init video

---
+++ src/main/java/com/amir/service/VideoService.java
@@ -0,0 +1,12 @@
+package com.amir.service;
+
+import com.amir.repository.VideoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VideoService {
+    @Autowired
+    private VideoRepository videoRepository;
+
+}
